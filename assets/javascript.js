9aa68edf0ad400ab94743448898e39be3e291370
danpoggetti <danpoggetti@gmail.com>
2022-11-15 19:44:27 -0800

    found save button solution on W3 tutorial

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,3 +6,14 @@ $(document).ready(function(){
         // This is the save button area which will also store to local storage
         let value = $(this).siblings('.description').val();
         let time = $(this).parent().attr('id');
+  
+        // Found this on w3 tutorial for the time, value mark
+        localStorage.setItem(time,value);
+    
+    $('.alert').addClass('show');
+    
+    setTimeout(function () {
+        $('.alert').removeClass('show');
+      }, 1000);
+    });
+    
